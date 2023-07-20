cb4b07d980f2abf908f9f04b1a731d59c8e61c7b
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-07-20 18:57:20 +0300

    add product models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import Product , ProducstImages , Brand , Category , ProductReview
 # Register your models here.
+
+
+
+class ProductImageTabular(admin.TabularInline):
+    model = ProducstImages
+
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageTabular]
+    list_display = ['name','flag','quantity','price']
+    list_filter = ['flag','brand','category']
+    search_fields = ['name','desc','subtitle']
+
+
+
+
+admin.site.register(Product ,ProductAdmin)
+admin.site.register(ProducstImages)
+admin.site.register(ProductReview)
+admin.site.register(Brand)
+admin.site.register(Category)
--- products/models.py
+++ products/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.utils.translation import gettext as _
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.core.validators import MinValueValidator , MaxValueValidator
+from taggit.managers import TaggableManager
+
 
 # Create your models here.
 
@@ -12,32 +17,54 @@ FLAG_OPTION=(
 
 
 
-class Products (models.Model):
+class Product (models.Model):
     name = models.CharField(max_length=100 , verbose_name=_("Name"))
     subtitle = models.CharField(_("Subtitle"),max_length=500)
     sku = models.IntegerField(_("SKU"))
     desc = models.TextField(_('Description'), max_length=10000)
-    price = models.models.FloatField(_("Price"))
+    price = models.FloatField(_("Price"))
     flag = models.CharField(_("Flag"), max_length=10 , choices=FLAG_OPTION)
     quantity = models.IntegerField(_("Quantity"))
-    brand =''
-    category = ''
+    brand = models.ForeignKey('Brand', related_name='product_brand',on_delete=models.SET_NULL, null=True , blank=True)
+    category = models.ForeignKey('Category', related_name='product_category',on_delete=models.SET_NULL, null=True , blank=True)
+    tags = TaggableManager()
+
+    def __str__(self):
+        return self.name
+    
 
 
 
 class ProducstImages(models.Model):
-    pass
+    product = models.ForeignKey(Product ,related_name='product_image', on_delete=models.CASCADE )
+    image = models.ImageField(upload_to='product_image/')
 
+    def __str__(self):
+        return str(self.product)
 
 
 class Brand(models.Model):
-    pass
+    name = models.CharField(_("Name"),max_length=50)
+    image = models.ImageField(_("Image"),upload_to='brands/')
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(_("Name"),max_length=50)
+    image = models.ImageField(_("Image"),upload_to='brands/')
 
+    def __str__(self):
+        return self.name
 
 
 class ProductReview(models.Model):
-    pass
+    user = models.ForeignKey(User, related_name='user_review',verbose_name="User" ,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product , related_name='product_review',verbose_name="Product" ,on_delete=models.CASCADE)
+    rate = models.IntegerField(_("Rate"), validators=[MaxValueValidator(5), MinValueValidator(0)])
+    review = models.IntegerField(_("Review"),max_length=500)
+    created_at = models.DateTimeField(_("Created at"),default=timezone.now)
+
+    def __str__(self):
+        return str(self.user)
