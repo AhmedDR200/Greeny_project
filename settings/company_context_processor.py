ece25abb3d1207d5b882132994ebeb6f2d13be32
Ahmed Magdy <94301115+AhmedDR200@users.noreply.github.com>
2023-07-21 00:12:11 +0300

    setup company model, context_processor, navbar

---
+++ settings/company_context_processor.py
@@ -0,0 +1,6 @@
+from .models import Company
+
+
+def get_info(request):
+    info = Company.objects.last()
+    return {'info':info}
--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    logo = models.ImageField(upload_to= 'company')
+    call_us = models.CharField(max_length=25)
+    email_us = models.EmailField()
+    subtitle = models.TextField(max_length=500)
+    fs_link = models.URLField(null=True , blank=True)
+    twit_link = models.URLField(null=True , blank=True)
+    insta_link = models.URLField(null=True , blank=True)
+    emails = models.TextField(max_length=100)
+    numbers = models.TextField(max_length=100)
+    address = models.TextField(max_length=100)
+    def __str__(self):
+        return self.name
+    
